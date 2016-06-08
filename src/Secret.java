bf4aa9f8102e77ea597476e7ffe0b1c560779a3a
Zakary French <radruler@gmail.com>
2016-06-07 21:40:26 -0700

    Added ChefFrank Support.

--- src/Secret.java
+++ src/Secret.java
@@ -3,59 +3,62 @@ import org.pircbotx.hooks.events.MessageEvent;
 
 
 public class Secret extends ListenerAdapter {
-	
+
 	public long lastCall = 0;
-	
+
 	public void onMessage(MessageEvent event) throws InterruptedException {
-		
+
 		String response = "";
 		int delay = 10000; // 10 seconds in between responses minimum
-		
+
 		if(event.getMessage().equals("Bob Ross")) {
-			event.getChannel().send().message("http://tf2bobross.deviantart.com/");		
+			event.getChannel().send().message("http://tf2bobross.deviantart.com/");
 		}
-		
+
 		if(event.getMessage().contains("FrankerZ")) {
 			response += "FrankerZ ";
 		}
-		
+
 		if(event.getMessage().contains("ZreknarF")) {
 			response += "ZreknarF ";
 		}
-		
+
 		if(event.getMessage().contains("LilZ")) {
 			response += "LilZ ";
 		}
-		
+
 		if(event.getMessage().contains("ZliL")) {
 			response += "ZliL ";
 		}
-		
+
 //		if(event.getMessage().toLowerCase().contains("washed taco")) {
 //			event.getChannel().send().message("http://bombch.us/jb8");
 //		}
-		
+
 		if(event.getMessage().contains("OhMyDog")) {
 			response += "OhMyDog ";
 		}
-		
+
 		if(event.getMessage().contains("RalpherZ")) {
 			response += "RalpherZ ";
 		}
-		
+
 		if(event.getMessage().contains("ZrehplaR")) {
 			response += "ZrehplaR ";
 		}
-		
+
 		if(event.getMessage().contains("CorgiDerp")) {
 			response += "CorgiDerp ";
 		}
-		
-		
+
+		if(event.getMessage().contains("ChefFrank")) {
+			response += "ChefFrank ";
+		}
+
 		if(System.currentTimeMillis() - lastCall >= delay) {
 			lastCall = System.currentTimeMillis();
 			event.getChannel().send().message(response);
 		}
-		
+
 	}
 }
